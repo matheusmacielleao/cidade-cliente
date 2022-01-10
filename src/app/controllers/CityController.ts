5a3e8e1abac1a5dc87e8c91c5d21c2f03a733471
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-10 10:10:47 -0300

    feat(controller): created city controller

---
+++ src/app/controllers/CityController.ts
@@ -0,0 +1,16 @@
+import {Request, Response} from "express";
+import { CityService} from "../services/CityService";
+
+const cityService = new CityService();
+
+export class CityController{
+    async handle(request: Request, response: Response){
+        try{
+            const result = await cityService.create(request.body);
+            return response.status(201).json(result);
+        } catch(err){
+            return response.status(400).json(err);
+        }
+    }
+
+}

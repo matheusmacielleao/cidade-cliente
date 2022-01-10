932e1f50d19a36c3b2df6e9d3d0fa51e932d541f
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-10 17:22:51 -0300

    feat(controller): client controller with create,find and delete methods

---
+++ src/app/controllers/ClientController.ts
@@ -0,0 +1,35 @@
+import {Request, Response} from 'express';
+
+import {ClientService} from '../services/ClientService';
+
+const clientService = new ClientService();
+
+class ClientController {
+  async create(request: Request, response: Response) {
+    try {
+      const result = await clientService.create(request.body);
+      return response.status(201).json(result);
+    } catch (err) {
+      return response.status(400).json(err);
+    }
+  }
+
+  async find(request: Request, response: Response) {
+    try {
+      const result = await clientService.find(request.query);
+      return response.status(201).json(result);
+    } catch (err) {
+      return response.status(400).json(err);
+    }
+  }
+  async delete(request: Request, response: Response) {
+    try {
+      await clientService.delete(request.params.id);
+      return response.status(204).json({});
+    } catch (err) {
+      return response.status(400).json(err);
+    }
+  }
+}
+
+export {ClientController};

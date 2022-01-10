e46325e1fc6edf713d5ebeb233252fa4e87cc0b4
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-10 11:17:54 -0300

    feat(cityRouter): route for find method

--- src/routes/city.router.ts
+++ src/routes/city.router.ts
@@ -4,5 +4,6 @@ import { CityController } from "../app/controllers/CityController";
 const cityRoutes = Router();
 
 cityRoutes.post("/cities", new CityController().create);
+cityRoutes.get("/cities", new CityController().find);
 
 export{cityRoutes}

9bfc98d2b88fa4f499faccafe708c6c050a9d9c5
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-10 17:24:32 -0300

    feat(database): client migration

---
+++ src/infra/database/migrations/1641844763459-CreateClients.ts
@@ -0,0 +1,46 @@
+import {MigrationInterface, QueryRunner, Table} from 'typeorm';
+
+export class CreateClients1641844763459 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+        new Table({
+          name: 'clients',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+            },
+            {
+              name: 'name',
+              type: 'varchar',
+            },
+            {
+              name: 'sex',
+              type: 'char',
+            },
+            {
+              name: 'birthdate',
+              type: 'date',
+            },
+            {
+              name: 'cityId',
+              type: 'uuid',
+            },
+          ],
+          foreignKeys: [
+            {
+              name: 'fk_client_city',
+              columnNames: ['cityId'],
+              referencedTableName: 'cities',
+              referencedColumnNames: ['id'],
+
+            },
+          ],
+        }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+}

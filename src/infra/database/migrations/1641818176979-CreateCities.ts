130ce37434941627747f593aa33109e20f60edfc
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-10 09:51:04 -0300

    feat(database): create Cities database migration

---
+++ src/infra/database/migrations/1641818176979-CreateCities.ts
@@ -0,0 +1,34 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateCities1641818176979 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name : "cities",
+                columns :[
+                    {
+                        name : "id",
+                        type : "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name : "name",
+                        type : "varchar",
+                        isUnique : true
+                    },
+                    {
+                        name:"state",
+                        type : "varchar"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("cities")
+
+    }
+
+}

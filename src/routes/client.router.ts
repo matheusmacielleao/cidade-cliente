dcbc8b9da54564da854f48c9712563d31c58848e
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-11 12:35:01 -0300

    feat(routes) : updateName route

--- src/routes/client.router.ts
+++ src/routes/client.router.ts
@@ -6,5 +6,7 @@ const clientRoutes = Router();
 clientRoutes.post('/clients', new ClientController().create);
 clientRoutes.get('/clients', new ClientController().find);
 clientRoutes.delete('/clients/:id', new ClientController().delete);
+clientRoutes.put('/clients/:id/updateName', new ClientController().updateName);
+
 
 export {clientRoutes};

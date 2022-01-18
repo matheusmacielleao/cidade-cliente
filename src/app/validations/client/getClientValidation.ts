217ba6c8671097d83c8bf15ccbbb1101be6688a9
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-18 09:46:25 -0300

    feat(joi_validation) : getClient joi validation

---
+++ src/app/validations/client/getClientValidation.ts
@@ -0,0 +1,23 @@
+import {Request, Response, NextFunction} from 'express';
+import Joi from 'joi';
+import {serialize} from '../../serializer/joiErrorSerializer';
+
+export = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const entity = Joi.object({
+      cityId: Joi.string().uuid().optional,
+      name: Joi.string().trim().optional,
+      gender: Joi.string().trim().optional,
+      birthdate: Joi.date().optional,
+
+    });
+
+    const {error} = entity.validate(req.query, {abortEarly: false});
+
+    if (error) throw error;
+
+    return next();
+  } catch (error) {
+    return res.status(400).json(serialize(error));
+  }
+};
--- src/routes/client.router.ts
+++ src/routes/client.router.ts
@@ -1,12 +1,13 @@
 import {Router} from 'express';
 import ClientController from '../app/controllers/ClientController';
 import createClientValidation from '../app/validations/client/createClientValidation';
+import getClientValidation from '../app/validations/client/getClientValidation';
 import updateClientValidation from '../app/validations/client/updateClientValidation';
 
 const clientRoutes = Router();
 
 clientRoutes.post('/clients', createClientValidation, ClientController.create);
-clientRoutes.get('/clients', ClientController.find);
+clientRoutes.get('/clients', getClientValidation, ClientController.find);
 clientRoutes.delete('/clients/:id', ClientController.delete);
 clientRoutes.put('/clients/:id/updateName', updateClientValidation, ClientController.updateName);
 

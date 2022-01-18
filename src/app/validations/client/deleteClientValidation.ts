c0434db0991c3e79aaa2d6ac9cb7c6a91e0f0ac7
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-18 13:08:18 -0300

    feat(validation): validation for delete client route

---
+++ src/app/validations/client/deleteClientValidation.ts
@@ -0,0 +1,19 @@
+import {Request, Response, NextFunction} from 'express';
+import Joi from 'joi';
+import {serialize} from '../../serializer/joiErrorSerializer';
+
+export = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const entity = Joi.object({
+      id: Joi.string().uuid().required(),
+    });
+
+    const {error} = entity.validate(req.params, {abortEarly: false});
+
+    if (error) throw error;
+
+    return next();
+  } catch (error) {
+    return res.status(400).json(serialize(error));
+  }
+};

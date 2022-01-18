919641f1c8c9d1749911b6420de34e500509521d
matheusmacielleao <matheusmacielleao@gmail.com>
2022-01-18 09:33:05 -0300

    refactor(database) : removed commentary

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,5 +1,4 @@
 import {createConnection} from 'typeorm';
-// createConnection(process.env.CONNECTION_NAME);
 export const connection = async () => {
   const connection = process.env.CONNECTION_NAME;
   await createConnection(connection+'');
